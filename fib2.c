8c6d80281b0f26178a2a2677db31d9939c19c174
extraGravee <singhsidak@ymail.com>
2019-08-05 11:01:32 +1000

    Added the less elegant but more effecient Fibonacci algorithm. Drops complexity from O(2^n) to O(n)

---
+++ fib2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int fib(int n);
+
+int main (int argc, char *argv[]){
+	int n, ans;
+
+	printf("\n Enter a numer to get it's fibonacci: ");
+	scanf("%d", &n);
+
+	ans = fib(n);
+
+	printf("\n The fibonacci of %d is %d. \n", n, ans);
+
+	return 0;
+}
+
+int fib(int n){
+	int nums[n+1];
+
+	// if (n==0) return 0;
+	// if (n==1) return 1;
+	nums[0] = 0;
+	nums[1] = 1;
+	for (int i=2; i<=n; i++){
+		nums[i] = nums[i-1] + nums[i-2];
+	}
+
+	return nums[n];
+}
