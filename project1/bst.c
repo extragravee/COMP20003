94262869bf9429a6d41c4e26d334228a5c9538c4
extragravee <42315179+extragravee@users.noreply.github.com>
2019-09-04 14:31:52 +1000

    Update bst.c

--- project1/bst.c
+++ project1/bst.c
@@ -3,7 +3,10 @@ Created by Sidakpreet Mann - 921322
 sidakpreetm@student.unimelb.edu.au
 
 Binary Search Tree implementation file
-Includes functions relating to construction, search and traversal of the bst dictionary
+Includes functions (performed on the bst dictionary):
+construction
+search
+traversal
 Function declerations and common constants abstracted to header file "bst.h"
 
 Code for the function 'insert_node' has been adapted from Worksheet3 and the authors of Worksheet 3 provided on JupyterHub
