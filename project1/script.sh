cb7f0217d4418bcacc7b10f8caf6fde73b465aec
extraGravy <singhsidak@ymail.com>
2019-09-05 20:44:37 +1000

    added in bash script to simulate bst performace

---
+++ project1/script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+for i in {200..100000..100}
+do
+	shuf -n $i 2018full_size_no_headers\ \(1\).csv > current.txt
+	shuf -n 100 current.txt | awk -F ',' '{print $16}' > input.txt
+	echo "$i------"
+	printf "$i," >> stats.csv
+	./dict1 current.txt output.txt < input.txt | awk -F ' --> ' '{sum+=$2} END {print sum/100}' >>stats.csv
+done
+
+
+
+
+
+# for i in {100..15000..100}
+# do
+# 	# echo "$i-----"
+# 	shuf -n 100 test.csv |\
+# 	awk -F ',' '{print $16}' > input.txt
+# 	echo "$i ------"
+# 	printf "$i," >> stats.csv
+# 	./dict1 test.csv output.txt < input.txt | awk -F ' --> ' '{sum+=$2} END {print sum/100}' >>stats.csv
+# done
