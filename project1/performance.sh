b126da496853e43f0292d7e9dc2e5f41694bab47
extraGravy <singhsidak@ymail.com>
2019-09-05 21:55:38 +1000

    performance.sh script working perfectly, results are good

--- project1/performance.sh
+++ project1/performance.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in {200..100000..100}
+for i in {100..100000..100}
 do
 	shuf -n $i 2018full_size_no_headers\ \(1\).csv > current.txt
 	shuf -n 100 current.txt | awk -F ',' '{print $16}' > input.txt
